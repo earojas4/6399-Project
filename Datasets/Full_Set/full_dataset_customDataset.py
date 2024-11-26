f331b4b535be7629d934fcdd4648031db95b4096
earojas4 <erojas4@utexas.edu>
2024-11-26 04:07:49 -0600

    Update and rename new to full_dataset_customDataset.py

---
+++ Datasets/Full_Set/full_dataset_customDataset.py
@@ -0,0 +1,6 @@
+combined_test_dataset = CustomDataset(
+    annotations_file='/content/Combined-1/test/_annotations.csv',
+    img_dir='/content/Combined-1/test',
+    transform=transform,
+    class_limit= {0:250, 1:None, 2:None, 3:None, 4:None, 5:None, 6:None, 7: None, 8:None, 9:None}
+)
