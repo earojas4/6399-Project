f87ef4caf88f971db5d74c4231bd089050313164
earojas4 <erojas4@utexas.edu>
2024-11-26 01:49:05 -0600

    Update roboflow.py

--- roboflow.py
+++ roboflow.py
@@ -1,1 +1,6 @@
-
+!pip install roboflow
+from roboflow import Roboflow
+rf = Roboflow(api_key="__________________________")
+project = rf.workspace("project-4ohwz").project("plant-diseases-detection-aerial")
+version = project.version(4)
+dataset = version.download("retinanet")
