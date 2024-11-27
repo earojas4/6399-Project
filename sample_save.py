7c1d3a5cebb149e9c275866a9f709d11c25340d7
earojas4 <erojas4@utexas.edu>
2024-11-26 23:41:39 -0600

    Create sample_save.py

---
+++ sample_save.py
@@ -0,0 +1,2 @@
+# the model referred to in this code is the model used in the training and validation loop, the second argument should be changed to the desired save location.
+torch.save(model, "resnet50_complete_model3_UAV.pth")
