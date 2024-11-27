82fe599ac26f889d2c9d588419d2dc9f8cdc914e
earojas4 <erojas4@utexas.edu>
2024-11-27 02:23:31 -0600

    Update sample_loader.py

--- Datasets/sample_loader.py
+++ Datasets/sample_loader.py
@@ -1,5 +1,6 @@
 # The first arguement within the Pytorch DataLoader should be the customDataset that corresponds with the target split.
-# Batch size should be adjusted based on available RAM. 
-# num_workers can be increased if the machine this code is run on can handle child operations while main operations are being run.
+# All photos will be uploaded in batches of 128 images to ensure that there is sufficient RAM to process the set.
+# Num workers is set to 0 when being run in a Colab environment. If run on a seperate machine, num workers can be increased to handle child operations if there is sufficient computational power.
+
 test_loader = DataLoader(combined_test_dataset, batch_size=128, shuffle=False, num_workers=0)
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=0)
